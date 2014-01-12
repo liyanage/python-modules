87ec98671acd726de6312722334d3156d783b009
Marc Liyanage <reg.gitcommit@entropy.ch>
2014-01-12 11:08:13 -0800

    Improve comment/documentation

--- tool_base.py
+++ tool_base.py
@@ -3,6 +3,43 @@
 #
 # Fancy command line tool skeleton
 #
+# If you want a single-file tool, you can either rename and adapt the
+# FooTool class at the bottom of this file, or even directly adapt the
+# Tool class and eliminate the subclass.
+#
+# In a larger project, you might want to maintain your tool subclass in
+# a separate file and keep this tool_base.py file unchanged so you can
+# easily update to never upstream versions. In that case, your subclass
+# file would look something like this:
+#
+#     #!/usr/bin/env python
+#
+#     from . import tool_base
+#     from . import my_module
+#
+#
+#     class MyTool(tool_base.Tool):
+#         """My useful tool"""
+#
+#         pass
+#
+#
+#     class SubcommandDoSomethign(tool_base.AbstractSubcommand):
+#         """Descripte the DoSomething subcommand"""
+#
+#         def run(self):
+#             print 'Hello world'
+#
+#         @classmethod
+#         def configure_argument_parser(cls, parser):
+#             parser.add_argument('path', help='Path to some useful file')
+#             parser.add_argument('-o', '--option', action='store_true', help='Some option')
+#
+#
+#     if __name__ == "__main__":
+#         MyTool.main()
+#
+#
 # Maintained at https://github.com/liyanage/python-modules
 #
 
@@ -111,7 +148,7 @@ class Tool(object):
         subcommand = non_option_arguments[0]
         if subcommand in subcommand_map.keys():
             return True
-        
+
         # converts a string like 'abc' to a regex like '(a).*?(b).*?(c)'
         regex = re.compile('.*?'.join(['(' + char + ')' for char in subcommand]))
         subcommand_candidates = []
@@ -120,7 +157,7 @@ class Tool(object):
             if not match:
                 continue
             subcommand_candidates.append(self.subcommand_candidate_for_abbreviation_match(subcommand_name, match))
-            
+
         if not subcommand_candidates:
             return True
 
@@ -128,7 +165,7 @@ class Tool(object):
             print >> sys.stderr, subcommand_candidates[0].decorated_name
             sys.argv[sys.argv.index(subcommand)] = subcommand_candidates[0].name
             return True
-        
+
         print >> sys.stderr, 'Ambiguous subcommand "{}": {}'.format(subcommand, ', '.join([i.decorated_name for i in subcommand_candidates]))
         return False
 
@@ -152,7 +189,7 @@ class Tool(object):
         subcommand_map = self.subcommand_map()
         if not self.resolve_subcommand_abbreviation(subcommand_map):
             exit(1)
-    
+
         parser = argparse.ArgumentParser(description=self.__doc__)
         self.configure_argument_parser(parser)
         parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose debug logging')
@@ -178,13 +215,13 @@ class Tool(object):
 
 class FooTool(Tool):
     """Describe Foo Tool"""
-    
+
     pass
-    
+
 
 class SubcommandBar(AbstractSubcommand):
     """List build settings that are defined in a project file, either at the project or target level."""
-    
+
     def run(self):
         print 'Hello world'
 
